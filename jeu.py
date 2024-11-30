5796b905a2e0bdbd4589ddd4d4e9163ee28bd800
nono-1117 <okahingaku@gmail.com>
2024-11-30 14:53:00 +0100

    Jsp ce que je fout ça m'pète les couilles

--- jeu.py
+++ jeu.py
@@ -2,6 +2,8 @@
 from typing import Literal
 
 from Canvas.hud_canvas import HUDCanvas
+from Perso.noble import Noble
+
 """
 TOUT TES IMPORT DES DIFFERENTES CLASSES DE PERSO
 """
@@ -10,15 +12,16 @@ class Jeu:
     def __init__(self, canvas: HUDCanvas):
         self.canvas = canvas
         """
-        TOUTES TES INSTANCIATIONS : LE JOUEUR (Seigneur), LES BOTS (Seigneur), ...
+        TOUTES TES INSTANCIATIONS : LE JOUEUR (Noble), LES BOTS (Noble), ...
         """
+        self.joueur = Noble("M.test",0,0)
         pass
 
     def immigrer(self, effectif: int, type_v: Literal["paysan", "artisan"], village_id: int):
         """
         Méthode qui va immigrer en fonction de canvas
 
-        :param effectif : nombre de du type de villageois désiré
+        :param effectif : nombre du type de villageois désiré
         :param type_v: type de villageois (PS : Literal["paysan", "artisan"] veut dire soit "paysan", soit "artisan" rien d'autre)
         :param village_id : l'id du village concerné
         """
