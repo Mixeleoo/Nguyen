b105b191750908c3a6c20828ea177bef037e4bf5
Mixeleoo <superrapace531@gmail.com>
2024-12-12 22:19:01 +0100

    Ajout des arguments geometry_width et geometry_height dans la méthode create d'un HUDABC.

--- Canvas/HUDs/HUDStandard/base.py
+++ Canvas/HUDs/HUDStandard/base.py
@@ -1,5 +1,5 @@
 
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from Canvas.HUDs.HUDMobile.base import HUDMobileABC
 from Canvas.hud_canvas import HUDCanvas
@@ -8,3 +8,7 @@ from .HUDAnimationManager import HUDAnimationManager
 class HUDABC(HUDAnimationManager, HUDMobileABC, ABC):
     def __init__(self, canvas: HUDCanvas):
         super().__init__(canvas)
+
+    @abstractmethod
+    def create(self, geometry_width: int, geometry_height: int):
+        pass
