438e36f1a79418e32294feeb0ab0e0479f9e00a3
Mixeleoo <superrapace531@gmail.com>
2024-12-12 13:24:48 +0100

    Le titre du sous HUD ChooseNoble dépend maintenant de l'instanciation.

--- Canvas/HUDs/SubHUD/SelectorInPage/HUDRadiobuttonInPage/choose_noble.py
+++ Canvas/HUDs/SubHUD/SelectorInPage/HUDRadiobuttonInPage/choose_noble.py
@@ -2,9 +2,11 @@
 from ..RadiobuttonInPageABC import RadiobuttonInPageABC
 
 class ChooseNoble(RadiobuttonInPageABC):
-    def __init__(self, canvas, hud_tag: str):
+    def __init__(self, canvas, hud_tag: str, title: str):
         super().__init__(canvas, hud_tag)
+
+        self._title = title
 
     @property
     def title(self):
-        return "Quel noble voulez-vous vassaliser ?"
+        return self._title
