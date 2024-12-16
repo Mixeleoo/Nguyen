5ad37ae8726965adaf4dddda27f6fd2308ffe010
Mixeleoo <superrapace531@gmail.com>
2024-12-16 11:30:19 +0100

    Le tag des HUD est maintenant automatique via self.__class__.__name__

--- Canvas/HUDs/HUDCentered/choose_noble_vassaliser.py
+++ Canvas/HUDs/HUDCentered/choose_noble_vassaliser.py
@@ -19,10 +19,6 @@ class ChooseNobleVassaliser(HUDCenteredABC):
         self.ok_button: Optional[Button] = None
         self.cancel_button: Optional[Button] = None
 
-    @property
-    def tag(self):
-        return "CHOOSE_NOBLE_VASSALISER"
-
     def create(self):
 
         x0_cadre, y0_cadre, x1_cadre, y1_cadre = self.choose_noble.create(0, 0)
